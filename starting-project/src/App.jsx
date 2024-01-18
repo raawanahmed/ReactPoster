87999864066908934dde0dcefb20e9d03dce3001
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2024-01-18 15:31:20 +0200

    add header

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import PostsList from "./components/PostsList";
+import MainHeader from './components/MainHeader';
+
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  function showModelHandler() {
+    setModalIsVisible(true);
+  }
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
   return (
-    <main>
-      <PostsList />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModelHandler} />
+      <main>
+        <PostsList
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
   );
 }
 
--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,26 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Post from './Post';
-import NewPost from './NewPost';
-import Modal from './Modal';
-import classes from './PostsList.module.css';
+import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import classes from "./PostsList.module.css";
 
-function PostsList() {
-  const [modalIsVisible, setModalIsVisible] = useState(true);
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
+function PostsList({ isPosting, onStopPosting }) {
+  // const [modalIsVisible, setModalIsVisible] = useState(true);
+  // const [enteredBody, setEnteredBody] = useState('');
+  // const [enteredAuthor, setEnteredAuthor] = useState('');
 
-  function hideModalHandler() {
-    setModalIsVisible(false);
-  }
+  // function hideModalHandler() {
+  //   setModalIsVisible(false);
+  // }
 
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
+  // function bodyChangeHandler(event) {
+  //   setEnteredBody(event.target.value);
+  // }
 
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
+  // function authorChangeHandler(event) {
+  //   setEnteredAuthor(event.target.value);
+  // }
 
   // let modalContent;
 
@@ -37,16 +37,12 @@ function PostsList() {
 
   return (
     <>
-      {modalIsVisible && (
-        <Modal onClose={hideModalHandler}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-          />
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Check out the full course!" />
       </ul>
     </>
